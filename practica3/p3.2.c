f8669d4a9c3fda55f559c888017c2d039ad3aac4
carlota <carlota.llealalonso@gmail.com>
2024-11-18 08:37:44 +0100

    programas

---
+++ practica3/p3.2.c
@@ -0,0 +1,8 @@
+int main(){
+int a ,b, c, d;
+b=2;
+c=3;
+d=4;
+a = b+c*d;
+a = d;
+}
---
+++ practica3/p3.6.c
@@ -0,0 +1,11 @@
+int main(){
+short a[4];
+short b[4];
+short c[4];
+a= [1, 2, 3, 4];
+b= [5, 6, 7, 8];
+
+while(i<4){
+    c[i]= a[i]*b[i];
+}
+}
